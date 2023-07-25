80c3104cf212ee9e345247f22ad5ead98a68b4f2
Lindsay Warr <iamalion@gmail.com>
2023-07-25 09:40:28 -0700

    Use helper action for HTML links

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,28 +1,35 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories/{categoryId}/items/new")]
-//     public ActionResult New(int categoryId)
-//     {
-//       Category category = Category.Find(categoryId);
-//       return View(category);
-//     }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
-//     public ActionResult Show(int categoryId, int itemId)
-//     {
-//       Item item = Item.Find(itemId);
-//       Category category = Category.Find(categoryId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("item", item);
-//       model.Add("category", category);
-//       return View(model);
-//     }
-//   }
-// }
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+        _db.Items.Add(item);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+  }
+}
