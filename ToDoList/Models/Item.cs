0962f3903b64680d016e2cf4cc2a36f053bcc8c2
Lindsay Warr <iamalion@gmail.com>
2023-07-25 15:36:22 -0700

    Add Categories views

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
-using MySqlConnector;
-
 namespace ToDoList.Models
 {
   public class Item
   {
     public int ItemId { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } 
 
   }
 }
